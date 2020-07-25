c802e2ada6dd12e5f134abeb8a4a3d545ff24a38
Nils Schönwald <nils@schoenwald.media>
2020-07-25 14:57:12 +0200

    update opening hours

--- app/components/OpeningHours/OpeningHours.jsx
+++ app/components/OpeningHours/OpeningHours.jsx
@@ -6,18 +6,26 @@ import { H3 } from '../Typography'
 const TableRow = styled.div`
   display: flex;
   justify-content: space-between;
+
+  & + & {
+    margin-top: 0.5em;
+  }
+
+  span:last-child {
+    text-align: right;
+  }
 `
 
 const OpeningHours = () => (
   <>
     <H3>Öffnungszeiten</H3>
     <TableRow>
-      <span>Mo. - Fr.</span>
-      <span>10:00 - 18:00</span>
+      <span>Mo.&nbsp;-&nbsp;Fr.</span>
+      <span>10:00&nbsp;-&nbsp;13:30, 14:30&nbsp;-&nbsp;18:00</span>
     </TableRow>
     <TableRow>
       <span>Sa.</span>
-      <span>11:00 - 14:00</span>
+      <span>11:00&nbsp;-&nbsp;14:00</span>
     </TableRow>
   </>
 )
